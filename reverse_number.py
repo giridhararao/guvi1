61ffbdb0e8bc6137efaec82793663c34f48816af
giridhararao <www.giridhararao@gmail.com>
2018-07-04 13:35:21 +0530

    Create reverse_number.py

---
+++ reverse_number.py
@@ -0,0 +1,18 @@
+a=int(input())
+ref=a
+ref1=a
+c=0
+arr[]
+while(a>0):
+  a=a//10
+  c=c+1
+for b in range(c):
+   arr[b]=a%10
+   a=a//10
+while (c-1)>=0:
+  print(arr[c-1])
+  c=c-1
+  
+  
+ 
+  
