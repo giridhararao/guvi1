11a992db1f8cd87499b50e5e0fad3ecdc2d87925
giridhararao <www.giridhararao@gmail.com>
2018-07-04 18:07:15 +0530

    Create non_repeated.py

---
+++ non_repeated.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=list()
+c=list()
+i=0
+temp=0
+for d in range(a)
+  b[d]=int(input())
+for e in range(a):
+  for f in range(a):
+      while e!=f:
+        if(b[e]==b[f]):
+          c[i]=b[f] 
+          i=i+1
+if(i==a):
+  c.sort()
+  print(c)
