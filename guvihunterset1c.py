9b9fa0a112ecc5cbd281eb8f199f1f2dfe5cb85c
giridhararao <www.giridhararao@gmail.com>
2018-07-16 23:23:11 +0530

    Update guvihunterset1c.py

--- guvihunterset1c.py
+++ guvihunterset1c.py
@@ -1,15 +1,26 @@
-a=int(input())
-b=list();
-c=list();
-i=0
-for c in range(a):
-  b[c]=int(input())
-for d in range(a):
-   if(b[d]==d):
-     c[i]=d
-     i=i+1
-if(i!=0):
-  c.sort()
-  print(c)
+n=int(raw_input())
+b=raw_input().split()
+d=len(b)
+s=[]
+count=0
+for i in range(0,d):
+	if(int(b[i])==i):
+		count=count+1
+		f=b[i]
+		s.append(f)
+e=len(s)
+p=[]
+if count>0:
+	for j in range(0,e):
+		largest=int(s[0])
+		g=len(s)
+		for i in range(1,g):
+			if(largest>int(s[i])):
+				largest=int(s[i])
+		p.append(largest)
+		s.remove(str(largest))
+	t=len(p)
+	for y in range(0,t):
+		print p[y],
 else:
-  print("-1")
+	print "-1"
