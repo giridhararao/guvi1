60a363af6a98eda02ca1589d881899337457d027
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-09-19 13:09:33 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,36 @@
+a=input()
+if a.isdigit():
+    l=[]
+    m=[]
+    z=[]
+    s=1
+    pt=[]
+    for i in range(0,len(a)):
+        l.append(a[i])
+    t=all(int(l[i]) <= int(l[i+1]) for i in range(len(l)-1))
+    if t==True:
+        temp=l[0]
+        l.remove(temp)
+        temp1=l[1]
+        l.remove(temp1)
+        l.append(str(temp))
+        l.append(str(temp1))
+        print("".join(l))
+    elif t==False:
+        r=all(int(l[i]) >= int(l[i+1]) for i in range(len(l)-1))
+        if r==True:
+            print("impossible")
+        else:
+            largest=l[0]
+            m=[]
+            for i in range(0,len(l)):
+                if largest<l[i]:
+                    m.append(l[i])
+            l.remove(largest)
+            for i in range(0,len(l)):
+                if l[i] not in m:
+                    m.append(l[i])
+            m.append(largest)
+            print ("".join(m))
+else:
+    print("invalid")
