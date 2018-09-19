8a35e9b40055864e81def12ac2df68872cc444e6
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-09-19 16:22:26 +0530

    Create sum pos or neg.py

---
+++ sum pos or neg.py
@@ -0,0 +1,22 @@
+n=int(raw_input())
+arr=raw_input().split()
+min=9999
+for i in range(0,n):
+	w=int(arr[i])
+	for j in range(0,n):
+		if i==j:
+			continue
+		else:
+			y=w+int(arr[j])
+			y=abs(y)
+			if min>y:
+				min=y
+				a=w
+				b=arr[j]
+if l>0:
+	print a,
+	print b
+else:
+	print b,
+	print a
+		
