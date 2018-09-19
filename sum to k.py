9b7ee330475dcbd13c96e36c9999222f96bc86b3
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-09-19 22:13:48 +0530

    Create sum to k.py

---
+++ sum to k.py
@@ -0,0 +1,34 @@
+a,d,b=input().split()
+if a.isdigit() and b.isdigit():
+	a=int(a)
+	b=int(b)
+	d=int(d)
+	z=[]
+	y=0
+	w=0
+	k=1
+	c=input().split()
+	for i in range(0,a):
+		if c[i].isdigit():
+			z.append(int(c[i]))
+		else:
+			w=w+1
+	for i in range(0,len(z)):
+		u=z[i]
+		s=0
+		for j in range(0,len(z)):
+			if i==j:
+				break
+			else:
+				s=u+z[j]
+				k=k+1
+				if s==b:
+					y=y+1
+	if w>0:
+		print("invalid")
+	elif(y>0):
+		print("YES")
+	elif y==0:
+		print("NO")
+else:
+        print("Invalid")
