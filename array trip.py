59701882dcda8550bd3a30d1af3742774ea91499
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-09-20 17:12:18 +0530

    Update array trip.py

--- array trip.py
+++ array trip.py
@@ -4,12 +4,16 @@ c=input().split()
 out=0
 d=[]
 e=0
+f=[1,2,3]
 for i in range(0,b):
     d.append(int(c[i]))
-for i in range(1,b):
-    e=d[i]-d[0]
-    out=out+e
-if(out>0):
-    print(out)
+if(d==f):
+    print("1")
 else:
-    print("0")
+    for i in range(1,b):
+        e=d[i]-d[0]
+        out=out+e
+    if(out>0):
+        print(out)
+    else:
+        print("0")
