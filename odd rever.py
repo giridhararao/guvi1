007e8dfaa0592825be278f54875292e103b63661
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-09-20 09:56:47 +0530

    Update odd rever.py

--- odd rever.py
+++ odd rever.py
@@ -1,6 +1,9 @@
 n=input().split()
 a=[]
 for i in range(0,len(n)):
+        if(i==(len(n)-1)):
+                if((n[i][-1])=='.'):
+                        n[i]=(n[i][:-1])
         if(i%2==0):
             b=n[i][::-1]
             a.append(b)
