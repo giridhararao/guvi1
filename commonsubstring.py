5fc77daf89ca29357ee5e5570b6e6ef792da0b11
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-09-21 14:58:10 +0530

    Create commonsubstring.py

---
+++ commonsubstring.py
@@ -0,0 +1,22 @@
+def lcs(S,T):
+    n = len(S)
+    m = len(T)
+    counter = [[0]*(m+1) for x in range(n+1)]
+    longest = 0
+    w= set()
+    for i in range(n):
+        for j in range(m):
+            if S[i] == T[j]:
+                y = counter[i][j] + 1
+                counter[i+1][j+1] = y
+                if y > longest:
+                    out=[]
+                    longest = y
+                    out.append(S[i-y+1:i+1])
+                elif y == longest:
+                    out.append(S[i-y+1:i+1])
+    out=out[::-1]
+
+    print(out[0])
+in1,in2=input().split()
+lcs(in1,in2)
