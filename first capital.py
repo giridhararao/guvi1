6eb42ea3f50e00619c8e8538da77eb5df53c5299
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-10-16 15:48:21 +0530

    Create first capital.py

---
+++ first capital.py
@@ -0,0 +1,27 @@
+a,b=input().split()
+z=len(a)
+y=len(b)
+u=a
+v=b
+e=[]
+f=[]
+g=[]
+for i in range(0,z):
+	if(i==0):
+		s=a[i].upper()
+		e.append(s)
+	elif(i>0):
+                s=a[i].lower()
+                e.append(s)
+n="".join(e)
+g.append(n)
+for i in range(0,y):
+	if(i==0):
+		s=b[i].upper()
+		f.append(s)
+	elif(i>0):
+                s=a[i].lower()
+                f.append(s)
+m="".join(f)
+g.append(m)
+print(" ".join(str(x) for x in g))
