f98adcd5dc10217144e89bf8e94722f7e76cab91
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-10-16 16:02:24 +0530

    Update one char differ.py

--- one char differ.py
+++ one char differ.py
@@ -4,13 +4,13 @@ d=len(b)
 out=0
 if(c==d):
         for i in range(0,c):
-		z=a[i]
-		y=b[i]
-		if(z==y):
-			out=0
-		else:
-			out=out+1
-	if(out==1):
+                z=a[i]
+                y=b[i]
+                if(z==y):
+                        out=0
+                else:
+                        out=out+1
+        if(out==1):
                 print("yes")
         else:
                 print("no")
