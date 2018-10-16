616b3c82ee0ae55a5522f62b36852f581ffa1a42
M.Giridhara Rao <www.giridhararao@gmail.com>
2018-10-16 15:57:45 +0530

    Create one char differ.py

---
+++ one char differ.py
@@ -0,0 +1,18 @@
+a,b=input().split()
+c=len(a)
+d=len(b)
+out=0
+if(c==d):
+        for i in range(0,c):
+		z=a[i]
+		y=b[i]
+		if(z==y):
+			out=0
+		else:
+			out=out+1
+	if(out==1):
+                print("yes")
+        else:
+                print("no")
+else:
+        print("no")
